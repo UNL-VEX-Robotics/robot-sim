3145b865b3545a594d57b1f2b412d431b97108ce
bclosman <bryceclosman@gmail.com>
2026-05-07 13:27:21 -0500

    Added include file for readability

---
+++ include/main.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <fstream>
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <unordered_map>
+
+#include "button.hpp"
+#include "font_manager.hpp"
+#include "simulation.hpp"
